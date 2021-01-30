aa755a791db2de657e4ecefc3ed142bee0755807
JupiterJun <hbbdmjz@163.com>
2021-01-30 08:57:17 +0800

    配置好环境了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import mdui from 'mdui'
+import 'mdui/dist/css/mdui.min.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
+Vue.use(mdui);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+module.exports = {
+  productionSourceMap: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 800,
+    disableHostCheck: true
+  },
+  chainWebpack: config => {
+    config.plugins.delete('prefetch')
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.mode = 'production'
+      return {
+        plugins: [new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 10240,
+          deleteOriginalAssets: false
+        })]
+      }
+    }
+  },
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: false,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 启用 CSS modules for all css / pre-processor files.
+    modules: false
+  }
+}
